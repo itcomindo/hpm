8f38640afa5f388d5d47123d8acd3b280ef6dc61
Budi Haryono <mail.budiharyono@gmail.com>
2024-02-13 18:48:46 +0700

    Refactor tag list display and add title attribute to tag links

--- inc/post-tags-inc.php
+++ inc/post-tags-inc.php
@@ -6,33 +6,16 @@
 
 defined('ABSPATH') or die('No script kiddies please!');
 
-function mm_get_post_tagsxxxx()
+function mm_get_post_tags()
 {
     $post_tags = get_the_tags();
     if ($post_tags) {
         echo '<div class="tags-title">';
         echo '<ul class="list-no-style tags-list">';
         foreach ($post_tags as $tag) {
-            echo '<li><a href="' . esc_url(get_tag_link($tag->term_id)) . '">' . esc_html($tag->name) . '</a></li>';
+            echo '<li><a href="' . esc_url(get_tag_link($tag->term_id)) . '" title="' . esc_html($tag->name) . '">' . esc_html($tag->name) . '</a></li>';
         }
         echo '</ul>';
         echo '</div>';
-    }
-}
-
-
-function mm_get_post_tags()
-{
-    if (isset($_SERVER['HTTP_USER_AGENT']) && preg_match('/Googlebot/', $_SERVER['HTTP_USER_AGENT'])) {
-        $post_tags = get_the_tags();
-        if ($post_tags) {
-            echo '<div class="tags-title">';
-            echo '<ul class="list-no-style tags-list">';
-            foreach ($post_tags as $tag) {
-                echo '<li><a href="' . esc_url(get_tag_link($tag->term_id)) . '">' . esc_html($tag->name) . '</a></li>';
-            }
-            echo '</ul>';
-            echo '</div>';
-        }
     }
 }
